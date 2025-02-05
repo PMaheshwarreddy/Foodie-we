0a7d4679442cf71a07b475a78cedbd41224fd386
Panduga MaheswarReddy <126186248+PMaheshwarreddy@users.noreply.github.com>
2025-02-05 22:07:18 +0530

    Update AddMenu.jsx

--- AddMenu.jsx
+++ AddMenu.jsx
@@ -5,6 +5,7 @@ import useAxiosPublic from "../../../hooks/useAxiosPublic";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import Swal from "sweetalert2";
 
+
 const AddMenu = () => {
   const { register, handleSubmit, reset } = useForm();
   const axiosPublic = useAxiosPublic();
