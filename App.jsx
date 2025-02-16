cb85d25823ccf4ccbfa09807f14bd59eefd6662b
Panduga MaheswarReddy <126186248+PMaheshwarreddy@users.noreply.github.com>
2025-02-16 21:46:58 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -2,10 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'https://github.com/PMaheshwarreddy/Foodie-we/blob/main/App.jsx#L9C0
-
-
-
-
 function App() {
   const [count, setCount] = useState(0)
 
